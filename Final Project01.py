48d762993499c6ccbc2461954e4bcaae92358940
HamzaOuadrassi <hamza.elkhroufouadrassi@gmail.com>
2025-03-15 15:57:47 +0000

    Add files via upload

---
+++ Final Project01.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #                                              The Final Project
+
+# ## Introduction to Data Science
+# 
+# - Data Science is an interdisciplinary field that combines statistical analysis, machine learning, and data engineering to extract meaningful insights from large and complex datasets. It leverages techniques from mathematics, programming, and domain knowledge to identify patterns, make predictions, and drive data-driven decisions. With the increasing availability of data, Data Science plays a crucial role in various industries such as healthcare, finance, marketing, and technology. By utilizing tools like Python, R, SQL, and machine learning algorithms, data scientists can solve complex problems and improve business outcomes.
+
+# ## List of Data Sience Languages
+# 
+# | Data sience Languages |
+# |---------------------- |
+# | Python |
+# | R |
+# | SQL |
+# | Java |
+# | Scala |
+# | Julia |
+
+# ## Data Science Libraries
+# 
+# | Languages | data science libraries |
+# |---------------------- |------------|
+# | Python |Pandas,NumPy,Matplotlib,Seaborn|
+# | R |ggplot2,dplyr,caret|
+
+# ## Data Science Tools
+# 
+# | Data Management Tools | Data Integration and Transformation Tools | Data Visualization Tools | Model Deployment Tools | Model Monitoring and Assessment Tools | Code Development and Execution Tools | Code Asset Management Tools | 
+# |---------------------- |--------------| -------------------------- | -------------------------- | ----------------------- | ----------- | ---------------------------- |
+# | MySQL | Apache Airflow | PixieDust | Apache PredictionIO | ModelDB | Jupyter IDE | Git | 
+# | PostgreSQL | Kubeflow | Hue | Kubernetes | Prometheus | RStudio | GitLab |
+# | Apache CouchDB | Apache Kafka | Kibana | Apache Seldon | IBM AI Fairness 360 | Microsoft Visual Studio | GitHub |
+# | MongoDB | Apache NiFi | Apache Superset | MLeap | IBM AI Explainability 360 | Anaconda Navigator | Bitbucket from Atlassian |
+# 
+
+# ## Arithmetic expression examples
+# 
+# - An Arithmetic Expression is a mathematical statement that combines numbers, variables, and operators to perform a calculation.
+# 
+# ### Simple Arithmetic Expressions Examples:
+# 
+# 1. Addition : 2+5
+# 2. Subtraction : 3-5
+# 3. Multiplication : 4*6
+# 4. Division : 8/2
+# 5. Modulus  : 10%3
+# 6. Exponentiation : 2**3
+# 7. Floor Division : 9//2
+
+# In[3]:
+
+
+## add number
+5+4
+
+
+# In[4]:
+
+
+## multiple
+7*4
+
+
+# In[6]:
+
+
+print ("hours = minutes/60")
+
+
+# ## Objectives
+# - Exercise 2 - Create a markdown cell with the title of the notebook. (1 pt)
+# - Exercise 3 - Create a markdown cell for an introduction. (1 pt)
+# - Exercise 4 - Create a markdown cell to list data science languages. (3 pts)
+# - Exercise 5 - Create a markdown cell to list data science libraries. (3 pts)
+# - Exercise 6 - Create a markdown cell with a table of Data Science tools. (3 pts)
+# - Exercise 7 - Create a markdown cell introducing arithmetic expression examples. (1 pt)
+# - Exercise 8 - Create a code cell to multiply and add numbers. (2 pts)
+# - Exercise 9 - Create a code cell to convert minutes to hours. (2 pts)
+# - Exercise 10 -Insert a markdown cell to list Objectives. (3 pts)
+# - Exercise 11 - Create a markdown cell to indicate the Author’s name. (2 pts)
+# - Exercise 12 - Share your notebook through GitHub (3 pts)
+# - Exercise 13 - Take a screenshot of the first page of the notebook. (1 pt)
+
+# ### Author:
+# #### Hamza El Khrouf Ouadrassi
+
+# In[ ]:
+
+
+
+
